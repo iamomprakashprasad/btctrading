495be3e35298c5400b38f8a72ddfe114c9108ca6
Om Prakash <om1504.iimt@gmail.com>
2023-04-09 15:28:04 +0530

    added test and staging workflows

---
+++ constants.py
@@ -0,0 +1,6 @@
+COINMARKETCAP_SANDBOX_DEVELOPER_BASE_URL = "sandbox.coinmarketcap.com"
+COINMARKETCAP_DEVELOPER_BASE_URL = "pro.coinmarketcap.com"
+
+COINGECKO_BASE_URL = "https://pro-api.coingecko.com/api/v3/"
+
+POLYGON_IO_BASE_URL = ""
